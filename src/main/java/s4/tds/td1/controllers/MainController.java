d3052e93f88aba27870dd39d5ae32c20adbec65e
jcheron <myaddressmail@gmail.com>
2020-01-20 23:49:59 +0100

    add Hello world

---
+++ src/main/java/s4/tds/td1/controllers/MainController.java
@@ -0,0 +1,13 @@
+package s4.tds.td1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	@GetMapping("")
+	public @ResponseBody String index() {
+		return "Hello world";
+	}
+}
---
+++ src/test/java/s4/tds/td1/Td1ApplicationTests.java
@@ -0,0 +1,27 @@
+package s4.tds.td1;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class Td1ApplicationTests {
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void loadHello() throws Exception {
+		this.mvc.perform(get("/")).andExpect(content().string("Hello world"));
+	}
+
+}
