1e6a027f094562e0f59bd77d43407f489b183982
jcheron <myaddressmail@gmail.com>
2020-01-21 12:00:33 +0100

    Update Td1ApplicationTests.java

--- src/test/java/s4/tds/td1/Td1ApplicationTests.java
+++ src/test/java/s4/tds/td1/Td1ApplicationTests.java
@@ -2,12 +2,15 @@ package s4.tds.td1;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,7 +24,7 @@ class Td1ApplicationTests {
 
 	@Test
 	void loadHello() throws Exception {
-		this.mvc.perform(get("/")).andExpect(content().string("Hello world!"));
+		ResultActions result = this.mvc.perform(get("/").contentType(MediaType.TEXT_PLAIN));
+		result.andExpect(status().isOk()).andExpect(content().string("Hello world!"));
 	}
-
 }
